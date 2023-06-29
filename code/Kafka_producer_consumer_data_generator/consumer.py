4c3dc594da112d2857988ac31663250e78d069d2
marcobrugin <marcobrugin00@gmail.com>
2023-06-29 16:32:40 +0200

    fix file python

--- code/Kafka_producer_consumer_data_generator/consumer.py
+++ code/Kafka_producer_consumer_data_generator/consumer.py
@@ -1,14 +1,14 @@
 from json import loads  
 from kafka import KafkaConsumer  
-
+import json
 
 consumer = KafkaConsumer(  
-    'test',  
+    'druid',  
      bootstrap_servers = ['localhost:9092', 'localhost:9093','localhost:9094'],  
      auto_offset_reset = 'earliest',  
      enable_auto_commit = True,  
      group_id = 'my-group',  
-     value_deserializer = lambda x : loads(x.decode('utf-8'))  
+     value_deserializer = lambda x : json.loads(x.decode('utf-8'))  
      )  
 for message in consumer:  
     message = message.value  
