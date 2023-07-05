bd733587759f9053c55653a45c3fbbbb13aa47be
marcobrugin <marcobrugin00@gmail.com>
2023-07-05 09:19:28 +0200

    fix

--- code/Rollup/producer.py
+++ code/Rollup/producer.py
@@ -10,22 +10,22 @@ producer = KafkaProducer(
     value_serializer = lambda x:json.dumps(x).encode('utf-8')  
     )  
 print("Conncet succefully") 
-cliente=['Mario','Luigi','Pippo','Pluto','Paperino','Paperone','Qui','Quo','Qua','Zio Paperone']
-prodotto=["primo", "secondo", "terzo", "quarto", "quinto", "sesto", "settimo", "ottavo", "nono", "decimo"]
+cliente=['Mario','Luigi']
+prodotto=["primo", "secondo"]
 volume=[]
 for n in range(100):  
-	for j in range(50):
+	for j in range(5):
 	    data=str(datetime.datetime.now())
 	    clienti=cliente[random.randint(0,len(cliente)-1)]
 	    prodotti= prodotto[random.randint(0,len(prodotto)-1)]
 	    quantita= random.randint(0,999)
-	    gradimento=random.randint(0, 10)
+	    gradimento=random.randint(0,10)
 	    my_data = {"timestamp": data, "cliente" : clienti, "prodotto": prodotti, "quantita": quantita, "gradimento": gradimento} 
 	    producer.send('rollup', value = my_data) 
 	    element=[data,clienti,prodotti,quantita,gradimento]
 	    volume.append(element)
 	print("Send")
-	sleep(2)
+	sleep(30)
 with open('data.csv', 'w', newline='') as file:
     writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC, delimiter=',')
     writer.writerows(volume) 
