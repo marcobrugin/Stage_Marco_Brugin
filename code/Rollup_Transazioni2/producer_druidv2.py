afc62692e21401948ed239c93b6863949f27f475
marcobrugin <marcobrugin00@gmail.com>
2023-07-24 15:08:06 +0200

    aggiunte query

--- code/Rollup_Transazioni2/producer_druidv2.py
+++ code/Rollup_Transazioni2/producer_druidv2.py
@@ -8,41 +8,33 @@ producer = KafkaProducer(
     bootstrap_servers = ["localhost:29092"],  
     value_serializer = lambda x:json.dumps(x).encode("utf-8")  
     )  
+
 print("Connect succefully") 
 fake = Faker()
-max_n=random.randint(4,4)
-max_c=random.randint(4,10)
-max_i=random.randint(4,10)
-#la libreria faker genera indirizzi con \n, che vanno sostituiti con uno spazio
-max_ci=random.randint(4,10)
-max_s=random.randint(4,10)
-max_r=random.randint(4,10)
-max_d=random.randint(4,10)
+locazione=[[fake.city(), fake.country()] for _ in range(200)]
 
+for i in range(20):
+ a=locazione[random.randint(0,99)]
+ citta=a[0]
+ stato=a[1] 
+ utenti=[[fake.first_name(), fake.last_name(),fake.date_of_birth(minimum_age=18, maximum_age=89).strftime("%Y-%m-%d"), citta, stato, fake.random_element(elements=("Scuola Secondaria", "Laurea triennale", "Laurea Magistrale", "Dottorato")), fake.random_element(elements=("Leggere","Viaggiare","Giocare a calcio","Giocare ai videogiochi","Fare sport")) ] for _ in range(150)]
 volume=[]
 for n in range(50):
-   nomi= [fake.first_name() for _ in range(max_n)]
-   cognomi= [fake.last_name() for _ in range(max_c)]
-   _citta=[fake.city() for _ in range(max_ci)]
-   stati=[fake.country() for _ in range(max_s)]
-   redditi= [round(random.uniform(1000, 10000), 2) for _ in range(max_r)]
-   daten= [fake.date_of_birth(minimum_age=18, maximum_age=89).strftime("%Y-%m-%d") for _ in range(max_d)]
+  
    for j in range(100000):
-        nome= random.choice(nomi)
-        cognome= random.choice(cognomi)
-        citta= random.choice(_citta)
-        stato= random.choice(stati)
-        reddito= random.choice(redditi)
-        datan= random.choice(daten)
-        istruzione= fake.random_element(elements=("Scuola Secondaria", "Laurea triennale", "Laurea Magistrale", "Dottorato"))
-        hobby= fake.random_element(elements=("Leggere","Viaggiare","Giocare a calcio","Giocare ai videogiochi","Fare sport"))
-        accesso= datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        my_data = {"Accesso": accesso, "nome": nome, "cognome": cognome,"citta": citta, "stato": stato,
-        "reddito":reddito, "datan":  datan, "istruzione": istruzione,
+        accesso=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        nome=utenti[random.randint(0,499)][0]
+        cognome=utenti[random.randint(0,499)][1]
+        datan=utenti[random.randint(0,499)][2]
+        citta=utenti[random.randint(0,499)][3]
+        stato=utenti[random.randint(0,499)][4]
+        istruzione=utenti[random.randint(0,499)][5]
+        hobby=utenti[random.randint(0,499)][6]
+        my_data = {"accesso": accesso, "nome": nome, "cognome": cognome, "datan":  datan, "citta": citta, "stato": stato, "istruzione": istruzione,
         "hobby": hobby
         }
-        producer.send("druid_rollup", value = my_data) 
-        element=[nome, cognome, citta, stato,reddito, datan,istruzione, hobby,accesso]
+        producer.send("rollup1", value = my_data) 
+        element=[nome, cognome, citta, stato, datan,istruzione, hobby,accesso]
         volume.append(element)
 with open('data.csv', 'w', newline='') as file:
     writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC, delimiter=',')
