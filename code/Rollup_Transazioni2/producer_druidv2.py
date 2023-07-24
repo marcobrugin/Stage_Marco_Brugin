a8f3be913143d0e04aa12b2f5dcc68cf994e32fd
marcobrugin <marcobrugin00@gmail.com>
2023-07-24 15:26:43 +0200

    correzione

--- code/Rollup_Transazioni2/producer_druidv2.py
+++ code/Rollup_Transazioni2/producer_druidv2.py
@@ -13,11 +13,12 @@ print("Connect succefully")
 fake = Faker()
 locazione=[[fake.city(), fake.country()] for _ in range(200)]
 
-for i in range(20):
- a=locazione[random.randint(0,99)]
- citta=a[0]
- stato=a[1] 
-utenti=[[fake.first_name(), fake.last_name(),fake.date_of_birth(minimum_age=18, maximum_age=89).strftime("%Y-%m-%d"), citta, stato, fake.random_element(elements=("Scuola Secondaria", "Laurea triennale", "Laurea Magistrale", "Dottorato")), fake.random_element(elements=("Leggere","Viaggiare","Giocare a calcio","Giocare ai videogiochi","Fare sport")) ] for _ in range(150)]
+utenti=list()
+for i in range(150):
+    a=locazione[random.randint(0,99)]
+    citta=a[0]
+    stato=a[1]
+    utenti.append([fake.first_name(), fake.last_name(),fake.date_of_birth(minimum_age=18, maximum_age=89).strftime("%Y-%m-%d"), citta, stato, fake.random_element(elements=("Scuola Secondaria", "Laurea triennale", "Laurea Magistrale", "Dottorato")), fake.random_element(elements=("Leggere","Viaggiare","Giocare a calcio","Giocare ai videogiochi","Fare sport")) ] )
 volume=[]
 for n in range(50):
   
@@ -33,12 +34,11 @@ for n in range(50):
         my_data = {"accesso": accesso, "nome": nome, "cognome": cognome, "datan":  datan, "citta": citta, "stato": stato, "istruzione": istruzione,
         "hobby": hobby
         }
-        producer.send("test_rollup1", value = my_data) 
+        producer.send("test_rollup2", value = my_data) 
         element=[nome, cognome, citta, stato, datan,istruzione, hobby,accesso]
         volume.append(element)
 with open('data.csv', 'w', newline='') as file:
     writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC, delimiter=',')
     writer.writerows(volume) 
-
+print(utenti)
 print("End")
-     
