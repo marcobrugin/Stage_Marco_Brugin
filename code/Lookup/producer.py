0cd371a2d0a4a479cb85cee293dd09f10a389bc2
marcobrugin <marcobrugin00@gmail.com>
2023-07-25 15:28:00 +0200

    correzione producer lookup

--- code/Lookup/producer.py
+++ code/Lookup/producer.py
@@ -8,7 +8,6 @@ producer = KafkaProducer(
     bootstrap_servers = ["localhost:29092"],  
     value_serializer = lambda x:json.dumps(x).encode("utf-8")  
     )  
-
 print("Connect succefully") 
 fake = Faker()
 locazione=[[fake.city(), fake.country()] for _ in range(200)]
@@ -20,6 +19,9 @@ for i in range(150):
     stato=a[1]
     a=random.randint(0,999)
     utenti.append([a, fake.first_name(), fake.last_name(),fake.date_of_birth(minimum_age=18, maximum_age=89).strftime("%Y-%m-%d"), citta, stato] )
+
+for i in range(150):
+ print('\"'+str(utenti[i][0])+"\""+":"+"\""+utenti[i][1]+" "+utenti[i][2]+"\"")
 volume=[]
 for n in range(500):
     accesso=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -29,13 +31,12 @@ for n in range(500):
     datan=utenti[random.randint(0,149)][3]
     citta=utenti[random.randint(0,149)][4]
     stato=utenti[random.randint(0,149)][5]
-    my_data = {"accesso": accesso, "codice_cliente": codice,  "datan":  datan, "citta": citta, "stato": stato,
+    my_data = {"accesso": accesso, "codice_cliente": str(codice),  "datan":  datan, "citta": citta, "stato": stato,
     }
-    producer.send("lookup", value = my_data) 
+    producer.send("lookup2", value = my_data) 
     element=[codice, nome, cognome, citta, stato, datan, accesso]
     volume.append(element)
 with open('data.csv', 'w', newline='') as file:
     writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC, delimiter=',')
     writer.writerows(volume) 
-print(utenti)
 print("End")
