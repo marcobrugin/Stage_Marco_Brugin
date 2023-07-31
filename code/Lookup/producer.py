93133a083fb199577bac76d9a026a71be87d6836
marcobrugin <marcobrugin00@gmail.com>
2023-07-31 09:47:15 +0200

    Corrette conclusioni rollup

--- code/Lookup/producer.py
+++ code/Lookup/producer.py
@@ -8,38 +8,35 @@ producer = KafkaProducer(
     bootstrap_servers = ["localhost:29092"],  
     value_serializer = lambda x:json.dumps(x).encode("utf-8")  
     )  
-
 print("Connect succefully") 
 fake = Faker()
 locazione=[[fake.city(), fake.country()] for _ in range(200)]
 
 utenti=list()
-for i in range(5000):
-    a=locazione[random.randint(0,199)]
+for i in range(150):
+    a=locazione[random.randint(0,99)]
     citta=a[0]
     stato=a[1]
-    utenti.append([fake.first_name(), fake.last_name(),fake.date_of_birth(minimum_age=18, maximum_age=89).strftime("%Y-%m-%d"), citta, stato, fake.random_element(elements=("Scuola Secondaria", "Laurea triennale", "Laurea Magistrale", "Dottorato")), fake.random_element(elements=("Leggere","Viaggiare","Giocare a calcio","Giocare ai videogiochi","Fare sport")) ] )
+    a=random.randint(0,999)
+    utenti.append([a, fake.first_name(), fake.last_name(),fake.date_of_birth(minimum_age=18, maximum_age=89).strftime("%Y-%m-%d"), citta, stato] )
+
+for i in range(150):
+ print('\"'+str(utenti[i][0])+"\""+":"+"\""+utenti[i][1]+" "+utenti[i][2]+"\"")
 volume=[]
-for n in range(50):
-  
-   for j in range(100000):
-        accesso=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        a=random.randint(0,4999)
-        nome=utenti[a][0]
-        cognome=utenti[a][1]
-        datan=utenti[a][2]
-        citta=utenti[a][3]
-        stato=utenti[a][4]
-        istruzione=utenti[a][5]
-        hobby=utenti[a][6]
-        my_data = {"accesso": accesso, "nome": nome, "cognome": cognome, "datan":  datan, "citta": citta, "stato": stato, "istruzione": istruzione,
-        "hobby": hobby
-        }
-        producer.send("test_rollup11", value = my_data) 
-        element=[nome, cognome, citta, stato, datan,istruzione, hobby,accesso]
-        volume.append(element)
+for n in range(500):
+    accesso=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    codice=utenti[random.randint(0,149)][0]
+    nome=utenti[random.randint(0,149)][1]
+    cognome=utenti[random.randint(0,149)][2]
+    datan=utenti[random.randint(0,149)][3]
+    citta=utenti[random.randint(0,149)][4]
+    stato=utenti[random.randint(0,149)][5]
+    my_data = {"accesso": accesso, "codice_cliente": str(codice),  "datan":  datan, "citta": citta, "stato": stato,
+    }
+    producer.send("lookup2", value = my_data) 
+    element=[codice, nome, cognome, citta, stato, datan, accesso]
+    volume.append(element)
 with open('data.csv', 'w', newline='') as file:
     writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC, delimiter=',')
     writer.writerows(volume) 
-print(utenti)
 print("End")
